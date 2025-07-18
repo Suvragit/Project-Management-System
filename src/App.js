f9debcf2722181d024d4fc0081b3f6021088ae1b
Tirthendu07 <tirthendumondal277@gmail.com>
2025-07-19 00:07:40 +0530

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <LandingPage />
     </div>
   );
 }
