7a9db5407d7ff69b984cd8d348f285352f3b7d52
shreyannandy-web <nandyshreyan2004@gmail.com>
2025-07-30 15:52:15 +0530

    Login page bug fixed

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,26 +1,75 @@
 import OptiAssign from '../Assets/OptiAssign.png'; 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import React,{useState} from 'react';
+import jsonData from '../Utility/Api.json'
 
 const Login =() =>{
+    const[loginType,setloginType]=useState('');
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
+    const[error,setError]=useState('');
+    const navigate=useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!loginType||!email||!password){
+        setError("All fields are required!");
+        return;
+        }
+        const userList=jsonData[loginType];
+       const matchedUser = jsonData.find(
+  (user) =>
+    user.role.toLowerCase() === loginType.toLowerCase() &&
+    user.email === email &&
+    user.password === password
+    );
+
+        if(matchedUser){
+            navigate('/Home');
+        }else{
+            setError('Invalid Email or Password');
+        }
+    };
+
     return(
         <div>
         <img src ={OptiAssign} className="w-72 h-72 mx-auto -mt-16 -mb-7" />
         <h2 className=" text-xl font-sans font-semibold grid place-items-center -mt-16 ">
             ALIGN | ASSIGN | ACHIEVE
             </h2>
-        <div className="bg-[#93CDDD] rounded-md  p-8 w-[430px] mx-auto mt-9 h-80 border-2 border-black">
-            <form className="grid place-items-center">
-                <select defaultValue="" className="w-[290px] rounded-md mb-10 h-[30px] pl-2 border-2 border-orange-400 ">
-                    <option value="" disabledhidden>Select Login Type</option>
+        <div className="bg-[#93CDDD] rounded-md  p-8 w-[430px] mx-auto mt-9 h-auto border-2 border-black">
+            <form className="grid place-items-center"
+            onSubmit={handleSubmit}>
+                <select value={loginType}
+                onChange={(e) => setloginType(e.target.value)}
+                 className="w-[290px] rounded-md mb-10 h-[30px] pl-2 border-2 border-orange-400 ">
+                    <option value="" disabled hidden>Select Login Type</option>
                     <option value="employee">Employee</option>
                     <option value="admin">Admin</option>
                 </select>
-                <input className="mx-10 mb-10 py-1 rounded-md w-[290px] pl-3 border-2 border-orange-400 " type="email" placeholder="Email"/>
-                <input className="mx-10 mb-10 py-1 rounded-md w-[290px] pl-3 border-2 border-orange-400 "  type="password" placeholder="Password" />
-                <Link to="/Home">
-                <button className=" bg-lime-500 w-[100px] mt-4 mx-20 rounded-md py-1 border border-black hover:bg-lime-600" type="submit"  >Login</button>
-                </Link>
+                <input className="mx-10 mb-10 py-1 rounded-md w-[290px] pl-3 border-2 border-orange-400 "
+                 type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+                <input className="mx-10 mb-10 py-1 rounded-md w-[290px] pl-3 border-2 border-orange-400 "
+                  type="password"
+                   placeholder="Password"
+                   value={password}
+                   onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {error && (
+            <p className="text-red-600 text-sm mt-1">{error}</p>
+          )}
+                
+                <button className=" bg-lime-500 w-[100px] mt-4 mx-20 rounded-md py-1 border border-black hover:bg-lime-600"
+                 type="submit">
+                    Login
+                   </button>
+                   
+                
             </form>
         </div>
         </div>
