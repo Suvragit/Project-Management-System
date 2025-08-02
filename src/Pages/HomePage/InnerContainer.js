c6ed5b12ddb6e942244e32bf74ebcb98e60a701c
sutirtha-bose222222 <bosesutirtha294@gmail.com>
2025-08-03 03:37:12 +0530

    Essential UI changes made

--- src/Pages/HomePage/InnerContainer.js
+++ src/Pages/HomePage/InnerContainer.js
@@ -3,7 +3,7 @@ import Cards from "./Cards";
 const InnerContainer = () => {
   return (
     <div className="flex-1 p-6 overflow-y-auto bg-white">
-      <h1 className="text-2xl font-bold mb-6 text-[#5A9CA0]">Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-6 text-[#9EBD5F]">Dashboard</h1>
       <div className="grid grid-cols-1  gap-6">
         <div className="lg:col-span-2">
           <Cards />
