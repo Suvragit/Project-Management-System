43f15877fe5e1228536f5c5418de986fb299a4e2
Suvragit <suvrajitdas63@gmail.com>
2025-08-02 21:36:34 +0530

    Enhancement done in Profile photo.

--- src/Component/Head/UserIcon.js
+++ src/Component/Head/UserIcon.js
@@ -1,12 +1,27 @@
-import Profilephoto from '../../Assets/profilephoto.jpg';
+import React, { useEffect, useState } from 'react';
+import jsonData from '../../Utility/Api.json';
 
 const UserIcon = () => {
+
+    const [photo, setPhoto]=useState("");
+    
+    useEffect(()=>{
+        const selectedemail=localStorage.getItem("email")
+        if(selectedemail){
+            const employee = jsonData.find(emp => emp.email === selectedemail);
+            if(employee){
+                setPhoto(employee.image);
+            }
+            
+        }
+
+    },[])
     return(
         <div className='w-12 h-12 border rounded-full border-white my-2 mx-2 overflow-hidden'>
             <img 
               className="w-full h-full object-cover" 
-              src={Profilephoto} 
-              alt="User profile"  // Add this
+              src={photo} 
+              alt="User profile"
             />
         </div>
     )
