08bd9168e37176fa8c36e5b5cce9496db61680ed
Tirthendu07 <tirthendumondal277@gmail.com>
2025-09-05 20:09:30 +0530

    Routing added  in admin and employee login type

--- src/Pages/HomePage/InnerContainer.js
+++ src/Pages/HomePage/InnerContainer.js
@@ -1,9 +1,10 @@
 import Cards from "./Cards";
 
 const InnerContainer = () => {
+  const loginType = localStorage.getItem("role");
   return (
     <div className="flex-1 p-6 overflow-y-auto bg-white">
-      <h1 className="text-2xl font-bold mb-6 text-[#9EBD5F]">Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-6 text-[#9EBD5F]">{loginType === "Admin" ? "Admin Dashboard" : "Employee Dashboard"}</h1>
       <div className="grid grid-cols-1  gap-6">
         <div className="lg:col-span-2">
           <Cards />
