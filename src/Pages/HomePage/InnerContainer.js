71f26fe4a754d31fa064cbfd45a971fae8466e3e
sutirtha-bose222222 <bosesutirtha294@gmail.com>
2025-09-14 00:58:36 +0530

    removed the Dashboard text

--- src/Pages/HomePage/InnerContainer.js
+++ src/Pages/HomePage/InnerContainer.js
@@ -8,7 +8,6 @@ const InnerContainer = () => {
   
   return (
     <div className="flex-1 p-6 overflow-y-auto bg-white">
-      <h1 className="text-2xl font-bold mb-6 text-[#9EBD5F]">Dashboard</h1>
       <div className="grid grid-cols-1 gap-6">
         <div className="lg:col-span-2">
           <Routes>
